38b3dd3f5d15e71f42de2e768f7ff629f76b0bff
chenzway <599031437@qq.com>
2018-07-02 09:22:13 +0800

    init vue and elementUI

---
+++ src/components/Todolist.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Todolist">
+    Todolist
+    <el-input v-model="item" placeholder="按回车即可添加!" @keyup.13.native='addItem'></el-input>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'Todolist',
+  data() {
+    return {
+      item
+    };
+  },
+  methods: {
+    addItem() {
+      alert(1);
+    }
+  }
+};
+</script>
